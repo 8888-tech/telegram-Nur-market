dc978872fa88331db6d314ad2ffdb3b35f51be64
Umidjon <boneskri@te-h10.tash.21-school.uz>
2026-07-09 04:20:56 +0500

    Yangilangan URL

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const URL = "https://script.google.com/macros/s/AKfycbyrNC9NJicBXzcu6VHIFoQ4fP8g2Xwded2hvfhU0EnNvWRcDkEU6snSb6mEVrxo7DL6Tw/exec";
+const URL = "https://script.google.com/macros/s/AKfycbyrNC9NJicBXzcu6VHIFoQ4fP8g2Xwded2hvfhU0EnNvWRcDkEU6snSb6mEVrxo7DL6Tw/execc";
 let allProducts = [], cart = [];
 
 document.addEventListener('DOMContentLoaded', loadProducts);
